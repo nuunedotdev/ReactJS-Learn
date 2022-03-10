bf9f56813ad33fba4b9b979f4d157eca2e997113
Abdulkadir Hassan <ahi.nuune@gmail.com>
2022-03-10 13:04:56 +0300

    Made Some Change on TopNavbar

--- src/components/app-layouts/TopNavbar.js
+++ src/components/app-layouts/TopNavbar.js
@@ -1,6 +1,9 @@
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function TopNavbar() {
+
+    const name = "Abdulkadir";
     return (
         <>
             <Navbar bg="light" expand="lg">
@@ -8,19 +11,27 @@ function TopNavbar() {
                     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <Nav.Link href="/add">Add</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
+
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link href="/add">Add</Nav.Link>
+                        </Nav>
+                        
                     </Navbar.Collapse>
+
+                    <Navbar.Collapse className="justify-content-end">
+
+                        <NavDropdown title={ name } id="basic-nav-dropdown">
+
+                            <NavDropdown.Item href="#action/3.2">My Account</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Change Password</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="https://google.com/"> Sign Out</NavDropdown.Item>
+                        </NavDropdown>
+                        
+                    </Navbar.Collapse>
+
                 </Container>
             </Navbar>
         </>
